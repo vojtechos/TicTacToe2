14db24488711a21f52aa0f865ca053601679ad1e
Vojtěch Vít <90755402+vojtechos@users.noreply.github.com>
2021-12-15 13:04:57 +0100

    Add files via upload

--- TicTacToe/src/com/company/Board.java
+++ TicTacToe/src/com/company/Board.java
@@ -5,12 +5,28 @@ public class Board {
    private Field[][] board;
 
    public Board(int m, int n) {
+      initBoard(m,n);
+   }
+
+   private void initBoard(int m, int n) {
+      Field[][] board = new Field[m][n];
+      for (int i = 0; i < board.length; i++) {
+         for (int j = 0; j < board[i].length; j++) {
+            board[i][j] = Field.EMPTYSPACE;
+         }
+      }
       this.board = board;
    }
 
+
    public void printBoard(){
 
+      for (int i = 0; i < board.length; i++) {
+         for (int j = 0; j < board[i].length; j++) {
+            System.out.print("_");
+         }
 
+      }
 
    }
 
